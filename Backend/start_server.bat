def6383f0774e1b9dc07b186e28df42a619d8248
Raghulskr12 <kalairaghul70@gmail.com>
2025-08-20 21:51:43 +0530

    [DELETE] Unwanted files removed

--- Backend/start_server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting AlloyMind FastAPI Server...
-echo =====================================
-echo.
-echo Server will be available at: http://localhost:8000
-echo Interactive docs at: http://localhost:8000/docs
-echo.
-python main.py
-pause
